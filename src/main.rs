40450493550042c88377c4651555739f08e999f2
Yunier Alvarez <yunieralvarez@gmail.com>
2023-10-21 18:24:36 -0400

    spawn_enemies_over_time

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .init_resource::<Score>()
         .init_resource::<StarSpawnTimer>()
+        .init_resource::<EnemySpawnTimer>()
         .add_startup_system(spawn_player)
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_enemies)
@@ -17,6 +18,8 @@ fn main() {
         .add_system(update_enemy_direction)
         .add_system(confine_enemy_movement)
         .add_system(enemy_hit_player)
+        .add_system(tick_enemy_spawn_time)
+        .add_system(spawn_enemies_over_time)
         .add_system(update_score)
         .add_system(tick_star_spawn_time)
         .add_system(spawn_stars_over_time)
@@ -293,6 +296,54 @@ pub fn enemy_hit_player(
     }
 }
 
+#[derive(Resource)]
+pub struct EnemySpawnTimer {
+    pub timer: Timer,
+}
+
+pub const ENEMY_SPAWN_TIME: f32 = 5.0;
+
+impl Default for EnemySpawnTimer {
+    fn default() -> Self {
+        EnemySpawnTimer {
+            timer: Timer::from_seconds(ENEMY_SPAWN_TIME, TimerMode::Repeating),
+        }
+    }
+}
+
+pub fn tick_enemy_spawn_time(mut enemy_spawn_timer: ResMut<EnemySpawnTimer>, time: Res<Time>) {
+    enemy_spawn_timer.timer.tick(time.delta());
+}
+
+pub fn spawn_enemies_over_time(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+    enemy_spawn_timer: ResMut<EnemySpawnTimer>,
+) {
+    if enemy_spawn_timer.timer.finished() {
+        let window = window_query.get_single().unwrap();
+        let random_x = random::<f32>() * window.width();
+        let random_y = random::<f32>() * window.height();
+
+        let sprite_bundle = SpriteBundle {
+            transform: Transform::from_xyz(random_x, random_y, 0.0),
+            texture: asset_server.load("sprites/ball_red_large.png"),
+            ..default()
+        };
+
+        let enemy = Enemy {
+            direction: Vec2::new(
+                random::<f32>() * (if random::<f32>() > 0.5 { 1.0 } else { -1.0 }),
+                random::<f32>() * (if random::<f32>() > 0.5 { 1.0 } else { -1.0 }),
+            )
+            .normalize(),
+        };
+
+        commands.spawn((sprite_bundle, enemy));
+    }
+}
+
 #[derive(Component)]
 pub struct Star {}
 
@@ -318,23 +369,6 @@ pub fn spawn_star(
 
         let star = Star {};
         commands.spawn((sprite_bundle, star));
-    }
-}
-
-#[derive(Resource)]
-pub struct Score {
-    pub value: u32,
-}
-
-impl Default for Score {
-    fn default() -> Self {
-        Score { value: 0 }
-    }
-}
-
-pub fn update_score(score: Res<Score>) {
-    if score.is_changed() {
-        println!("Score: {}", score.value.to_string());
     }
 }
 
@@ -376,5 +410,22 @@ pub fn spawn_stars_over_time(
 
         let star = Star {};
         commands.spawn((sprite_bundle, star));
+    }
+}
+
+#[derive(Resource)]
+pub struct Score {
+    pub value: u32,
+}
+
+impl Default for Score {
+    fn default() -> Self {
+        Score { value: 0 }
+    }
+}
+
+pub fn update_score(score: Res<Score>) {
+    if score.is_changed() {
+        println!("Score: {}", score.value.to_string());
     }
 }
