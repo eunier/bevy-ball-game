3449082473d307e1a0295ed47ebf175b863d9a9b
Yunier Alvarez <yunieralvarez@gmail.com>
2023-10-21 10:43:29 -0400

    rename

--- src/main.rs
+++ src/main.rs
@@ -1,105 +1,16 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PrimaryWindow};
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(PeoplePlugin)
-        .run();
-}
-
-pub struct PeoplePlugin;
-
-impl Plugin for PeoplePlugin {
-    fn build(&self, app: &mut App) {
-        app.add_startup_system(setup)
-            .add_system(print_names)
-            .add_system(people_with_jobs)
-            .add_system(people_ready_for_hire)
-            .add_system(person_does_job);
-    }
-}
-
-pub fn setup(mut commands: Commands) {
-    commands.spawn((
-        Person {
-            name: "Alex".to_string(),
-        },
-        Employed { job: Job::Doctor },
-    ));
-
-    commands.spawn(Person {
-        name: "Bob".to_string(),
-    });
-
-    commands.spawn((
-        Person {
-            name: "Charlie".to_string(),
-        },
-        Employed {
-            job: Job::FireFighter,
-        },
-    ));
-
-    commands.spawn((
-        Person {
-            name: "David".to_string(),
-        },
-        Employed { job: Job::Lawyer },
-    ));
-
-    commands.spawn((
-        Person {
-            name: "Ellen".to_string(),
-        },
-        Employed {
-            job: Job::FireFighter,
-        },
-    ));
-}
-
-pub fn print_names(person_query: Query<&Person>) {
-    for person in person_query.iter() {
-        println!("name: {}", person.name)
-    }
-}
-
-pub fn people_with_jobs(person_query: Query<&Person, With<Employed>>) {
-    for person in person_query.iter() {
-        println!("{} has a job.", person.name);
-    }
-}
-
-pub fn people_ready_for_hire(person_query: Query<&Person, Without<Employed>>) {
-    for person in person_query.iter() {
-        println!("{} ready for hire.", person.name);
-    }
-}
-
-pub fn person_does_job(person_query: Query<(&Person, &Employed)>) {
-    for (person, employed) in person_query.iter() {
-        let job_name = match employed.job {
-            Job::Doctor => "Doctor",
-            Job::FireFighter => "FireFighter",
-            Job::Lawyer => "Lawyer",
-        };
-
-        println!("{0} is a {1}", person.name, job_name);
-    }
+    App::new().add_plugins(DefaultPlugins).run();
 }
 
 #[derive(Component)]
-pub struct Person {
-    pub name: String,
-}
+pub struct Player {}
 
-#[derive(Component)]
-pub struct Employed {
-    pub job: Job,
-}
-
-#[derive(Debug)]
-pub enum Job {
-    Doctor,
-    FireFighter,
-    Lawyer,
+pub fn spawn_player(
+    mut commands: Commands,
+    windows_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = windows_query.get_single().unwrap();
 }
