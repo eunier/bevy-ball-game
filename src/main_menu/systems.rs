f0be6198df26a29c4a8f76cf5260983732bf1f8c
Yunier Alvarez <yunieralvarez@gmail.com>
2023-10-22 17:00:41 -0400

    wip

--- src/main_menu/systems.rs
+++ src/main_menu/systems.rs
@@ -1,3 +1,12 @@
-pub fn main_menu() {
-    println!("main menu")
+use bevy::prelude::*;
+
+pub fn build_main_menu(commands: &mut Commands, asset_server: &Res<AssetServer>) -> Entity {
+    let main_menu_entity = commands
+        .spawn(NodeBundle {
+            background_color: Color::RED.into(),
+            ..default()
+        })
+        .id();
+
+    main_menu_entity
 }
