cdf08cca932d882fa0ddbd3d10c34914165ddbfa
Yunier Alvarez <yunieralvarez@gmail.com>
2023-10-22 12:56:49 -0400

    use run condition

--- src/app/plugins.rs
+++ src/app/plugins.rs
@@ -1,11 +1,16 @@
 use bevy::prelude::*;
 
-use super::states::AppState;
+use super::{
+    states::AppState,
+    systems::{transition_to_game_state, transition_to_main_menu_state},
+};
 
 pub struct AppStatePlugin;
 
 impl Plugin for AppStatePlugin {
     fn build(&self, app: &mut App) {
-        app.add_state::<AppState>();
+        app.add_state::<AppState>()
+            .add_system(transition_to_game_state)
+            .add_system(transition_to_main_menu_state);
     }
 }
---
+++ src/app/systems.rs
@@ -0,0 +1,29 @@
+use bevy::prelude::*;
+
+use super::states::AppState;
+
+pub fn transition_to_game_state(
+    mut commands: Commands,
+    keyboard_input: Res<Input<KeyCode>>,
+    app_state: Res<State<AppState>>,
+) {
+    if keyboard_input.just_pressed(KeyCode::G) {
+        if app_state.0 != AppState::Game {
+            commands.insert_resource(NextState(Some(AppState::Game)));
+            println!("Entered AppState::Game");
+        }
+    }
+}
+
+pub fn transition_to_main_menu_state(
+    mut commands: Commands,
+    keyboard_input: Res<Input<KeyCode>>,
+    app_state: Res<State<AppState>>,
+) {
+    if keyboard_input.just_pressed(KeyCode::M) {
+        if app_state.0 != AppState::MainMenu {
+            commands.insert_resource(NextState(Some(AppState::MainMenu)));
+            println!("Entered AppState::MainMenu");
+        }
+    }
+}
