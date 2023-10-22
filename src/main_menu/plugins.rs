f1b6170279b6f201891e06790f0690f6862aa1a0
Yunier Alvarez <yunieralvarez@gmail.com>
2023-10-22 17:17:43 -0400

    style

--- src/main_menu/plugins.rs
+++ src/main_menu/plugins.rs
@@ -1,7 +1,14 @@
 use bevy::prelude::*;
 
+use crate::app::states::AppState;
+
+use super::systems::{despawn_main_menu, spawn_main_menu};
+
 pub struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_system(spawn_main_menu.in_schedule(OnEnter(AppState::MainMenu)))
+            .add_system(despawn_main_menu.in_schedule(OnExit(AppState::MainMenu)));
+    }
 }
--- src/main_menu/systems.rs
+++ src/main_menu/systems.rs
@@ -1,8 +1,18 @@
 use bevy::prelude::*;
 
+pub fn spawn_main_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let main_menu_entity = build_main_menu(&mut commands, &asset_server);
+}
+
+pub fn despawn_main_menu(mut commands: Commands, asset_server: Res<AssetServer>) {}
+
 pub fn build_main_menu(commands: &mut Commands, asset_server: &Res<AssetServer>) -> Entity {
     let main_menu_entity = commands
         .spawn(NodeBundle {
+            style: Style {
+                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                ..default()
+            },
             background_color: Color::RED.into(),
             ..default()
         })
