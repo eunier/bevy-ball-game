5bb6fdf0c13ad6e2385f7edd2856a606f4b90f5d
Yunier Alvarez <yunieralvarez@gmail.com>
2023-10-22 16:14:25 -0400

    state

--- src/game_over/systems.rs
+++ src/game_over/systems.rs
@@ -1,8 +1,12 @@
-use super::events::GameOver;
 use bevy::prelude::*;
 
-pub fn handle_game_over(mut game_over_event_reader: EventReader<GameOver>) {
+use crate::app::states::AppState;
+
+use super::events::GameOver;
+
+pub fn handle_game_over(mut commands: Commands, mut game_over_event_reader: EventReader<GameOver>) {
     for event in game_over_event_reader.iter() {
+        commands.insert_resource(NextState(Some(AppState::GameOver)));
         println!("Your final score is: {}", event.score.to_string());
     }
 }
