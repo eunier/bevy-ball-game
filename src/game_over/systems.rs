1a26c6b723fe0ab55235543ece47e04d74f05894
Yunier Alvarez <yunieralvarez@gmail.com>
2023-10-22 16:22:48 -0400

    refactor next state

--- src/game_over/systems.rs
+++ src/game_over/systems.rs
@@ -4,9 +4,12 @@ use crate::app::states::AppState;
 
 use super::events::GameOver;
 
-pub fn handle_game_over(mut commands: Commands, mut game_over_event_reader: EventReader<GameOver>) {
+pub fn handle_game_over(
+    mut game_over_event_reader: EventReader<GameOver>,
+    mut next_app_state: ResMut<NextState<AppState>>,
+) {
     for event in game_over_event_reader.iter() {
-        commands.insert_resource(NextState(Some(AppState::GameOver)));
+        next_app_state.set(AppState::GameOver);
         println!("Your final score is: {}", event.score.to_string());
     }
 }
--- src/simulation/systems.rs
+++ src/simulation/systems.rs
@@ -3,18 +3,18 @@ use bevy::prelude::*;
 use super::states::SimulationState;
 
 pub fn toggle_simulation(
-    mut commands: Commands,
+    mut next_simulation_state: ResMut<NextState<SimulationState>>,
     keyboard_input: Res<Input<KeyCode>>,
     simulation_state: Res<State<SimulationState>>,
 ) {
     if keyboard_input.just_pressed(KeyCode::Space) {
         if simulation_state.0 == SimulationState::Running {
-            commands.insert_resource(NextState(Some(SimulationState::Paused)));
+            next_simulation_state.set(SimulationState::Paused);
             println!("Simulation Paused.");
         }
 
         if simulation_state.0 == SimulationState::Paused {
-            commands.insert_resource(NextState(Some(SimulationState::Running)));
+            next_simulation_state.set(SimulationState::Running);
             println!("Simulation Running.");
         }
     }
