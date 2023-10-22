7e5ada702adea6a8c5dcbd141448c2cbde97eac4
Yunier Alvarez <yunieralvarez@gmail.com>
2023-10-22 17:52:14 -0400

    wip menu

--- src/main_menu.rs
+++ src/main_menu.rs
@@ -1,3 +1,4 @@
 pub mod components;
+pub mod constants;
 pub mod plugins;
 pub mod systems;
--- src/main_menu/systems.rs
+++ src/main_menu/systems.rs
@@ -1,20 +1,77 @@
 use bevy::prelude::*;
 
+use super::{
+    components::{MainMenu, PlayButton, QuitButton},
+    constants::{BUTTON_STYLE, NORMAL_BUTTON_COLOR},
+};
+
 pub fn spawn_main_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let main_menu_entity = build_main_menu(&mut commands, &asset_server);
+    build_main_menu(&mut commands, &asset_server);
 }
 
-pub fn despawn_main_menu(mut commands: Commands, asset_server: Res<AssetServer>) {}
+pub fn despawn_main_menu(mut commands: Commands, main_menu_query: Query<Entity, With<MainMenu>>) {
+    if let Ok(main_menu_entity) = main_menu_query.get_single() {
+        commands.entity(main_menu_entity).despawn_recursive();
+    }
+}
 
 pub fn build_main_menu(commands: &mut Commands, asset_server: &Res<AssetServer>) -> Entity {
     let main_menu_entity = commands
-        .spawn(NodeBundle {
-            style: Style {
-                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+        .spawn((
+            NodeBundle {
+                style: Style {
+                    align_content: AlignContent::Center,
+                    flex_direction: FlexDirection::Column,
+                    justify_content: JustifyContent::Center,
+                    size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                    ..default()
+                },
+                background_color: Color::RED.into(),
                 ..default()
             },
-            background_color: Color::RED.into(),
-            ..default()
+            MainMenu {},
+        ))
+        .with_children(|parent| {
+            // title
+            // play
+            parent
+                .spawn((
+                    ButtonBundle {
+                        style: BUTTON_STYLE,
+                        background_color: NORMAL_BUTTON_COLOR.into(),
+                        ..default()
+                    },
+                    PlayButton {},
+                ))
+                .with_children(|parent| {
+                    parent.spawn(TextBundle {
+                        text: Text {
+                            sections: vec![TextSection::new(
+                                "Play",
+                                TextStyle {
+                                    font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                    font_size: 32.0,
+                                    color: Color::WHITE,
+                                },
+                            )],
+                            alignment: TextAlignment::Center,
+                            ..default()
+                        },
+                        ..default()
+                    });
+                });
+            // quit
+            parent.spawn((
+                ButtonBundle {
+                    style: Style {
+                        size: Size::new(Val::Px(200.0), Val::Px(80.0)),
+                        ..default()
+                    },
+                    background_color: NORMAL_BUTTON_COLOR.into(),
+                    ..default()
+                },
+                QuitButton {},
+            ));
         })
         .id();
 
