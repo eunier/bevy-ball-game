d4bef8b8425b59dafc54d079bc07d06f2fb1d607
Yunier Alvarez <yunieralvarez@gmail.com>
2023-10-22 13:08:45 -0400

    spawn enemies

--- src/enemy/plugins.rs
+++ src/enemy/plugins.rs
@@ -1,4 +1,6 @@
-use bevy::prelude::Plugin;
+use bevy::prelude::{IntoSystemAppConfig, IntoSystemConfigs, OnEnter, OnUpdate, Plugin};
+
+use crate::{app::states::AppState, simulation::states::SimulationState};
 
 use super::{
     resources::EnemySpawnTimer,
@@ -13,12 +15,18 @@ pub struct EnemyPlugin;
 impl Plugin for EnemyPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
         app.init_resource::<EnemySpawnTimer>()
-            .add_startup_system(spawn_enemies)
-            .add_system(enemy_movement)
-            .add_system(update_enemy_direction)
-            .add_system(confine_enemy_movement)
-            .add_system(enemy_hit_player)
-            .add_system(tick_enemy_spawn_time)
-            .add_system(spawn_enemies_over_time);
+            .add_system(spawn_enemies.in_schedule(OnEnter(AppState::Game)))
+            .add_systems(
+                (
+                    enemy_movement,
+                    update_enemy_direction,
+                    confine_enemy_movement,
+                    enemy_hit_player,
+                    tick_enemy_spawn_time,
+                    spawn_enemies_over_time,
+                )
+                    .in_set(OnUpdate(AppState::Game))
+                    .in_set(OnUpdate(SimulationState::Running)),
+            );
     }
 }
